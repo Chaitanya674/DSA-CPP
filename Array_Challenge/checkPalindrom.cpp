7735b0bc1a74cd080ab37b4536e5e1807d2adb5e
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-09-11 23:12:43 +0530

    DSA CPP

--- Array_Challenge/checkPalindrom.cpp
+++ Array_Challenge/checkPalindrom.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main(){
-    
+    //check Palimdrone
+    int n;
+    cin>>n;
+
+    char arr[n+1];
+    bool check = 1;
+    for(int i = 0 ; i < n ; i++){
+        if(arr[i] != arr[n-i]){
+            check = 0;
+        }
+    }
+    return check;
 }

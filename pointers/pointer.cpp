4eee08ddb3ffd87161fe7725d294ee40f8f70b74
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-09-14 00:19:47 +0530

    DSA CPP

--- pointers/pointer.cpp
+++ pointers/pointer.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
 using namespace std;
 
+void increment(int a){
+    a++;
+}
+
 int main(){
-    int a = 0;
+    int a = 10;
     int* aptr = &a;
+    cout<<*aptr<< endl;
+    cout<<&a<< endl;
+
+    *aptr = 11;
+    cout<<&a<< endl;
+
+    aptr++;
+    cout<<aptr<<endl;
+    cout<<&a<< endl;
+
+    int arr[] = {10, 20 ,30 };
+    cout<< *arr <<endl;
+
+    int *ptr = arr;
+    //one way to traverse
+    // for(int i = 0; i< 3; i++){
+    //     cout<<*ptr<<endl;
+    //     ptr++;
+    // }
+
+    //second way
+    for(int i = 0 ; i< 3; i++){
+        cout<<*(arr+i)<<endl;
+        ////arr++; // illegal
+    }
+
+    increment(a);
+    cout << &a << endl;
+    cout << a << endl;
+
+    return 0;
+
+
 }

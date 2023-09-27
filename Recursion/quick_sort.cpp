a280682cfc3ff4be73d00f51b443a10b9fc2de49
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-09-27 23:54:10 +0530

    DSA-CPP

--- Recursion/quick_sort.cpp
+++ Recursion/quick_sort.cpp
@@ -2,9 +2,48 @@
 
 using namespace std;
 
+int partition(int arr[] , int start , int end ){
+    int pivot = arr[start];
+    int count = 0;
+    for(int i = start+1; i <= end; i++){
+        if(arr[i] <= pivot){
+            count++;
+        }
+    }
+    //place pivot at right place 
+    int pivotIndex = start+ count;
+    swap(arr[pivotIndex] , arr[start]);
+
+    //left and right part
+    int i = start, j = end; 
+    while(i < pivotIndex && j > pivotIndex){
+        while(arr[i]< pivot){
+            i++;
+        }
+        while(arr[j] > pivot){
+            j--;
+        }
+
+        if (i < pivotIndex && j > pivotIndex){
+            swap(arr[i++] , arr[j--]);
+        }
+
+    }
+
+    return pivotIndex;
+}
 void quickSort(int *arr , int start , int end){
+    //base case
+    if (start >= end){
+        return; 
+    }
+    int pivot = partition(arr , start , end);
+    //partitions in quick for sort 
+    //recursion 
+    quickSort(arr , start , pivot-1);
     
-    return; 
+    quickSort(arr , pivot+1 , end);
+    //merge the lists 
 }
 
 int main(){

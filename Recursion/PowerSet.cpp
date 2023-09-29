71872e511e6fa09c04af521876ee2f50bef3ee03
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-09-29 18:00:33 +0530

    DSA-CPP

---
+++ Recursion/PowerSet.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace  std;
+
+int main(){
+    //in power set the number of element are 2^n
+    //using recurrsion is about excluding an element or including an element 
+    
+    return 0 ;
+}

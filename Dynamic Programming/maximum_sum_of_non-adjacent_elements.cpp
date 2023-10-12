13ac304dfa1229fb43d3644e56c2bf7375776d6e
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-10-12 22:45:13 +0530

    DSA CPP

--- Dynamic Programming/maximum_sum_of_non-adjacent_elements.cpp
+++ Dynamic Programming/maximum_sum_of_non-adjacent_elements.cpp
@@ -51,6 +51,25 @@ int solver_tabulation(int *arr, int n  , vector<int> &dp){
     return dp[n-1];
 }
 
+//Space Optimization 
+int solver_tabulation(int *arr, int n  , vector<int> &dp){
+    if (n < 0 ){
+        return 0 ;
+    }
+    if (n == 0){
+        return arr[0];
+    }
+    if (dp[n] != -1){
+        return dp[n];
+    }
+    for(int i  = 1 ; i < n ; i++){
+        int incl = dp[i -2] + arr[i];
+        int excl = dp[i-1] + 0;
+        dp[i] = max(incl , excl);
+    }
+    return dp[n-1];
+}
+
 int main(){
     int arr[] = {9 , 9 , 8, 2};
     int n = 4;

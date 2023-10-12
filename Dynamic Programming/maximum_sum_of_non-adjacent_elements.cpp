2545bb5a9f063532bf353fd8bfb0885aa5236544
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-10-12 18:08:05 +0530

    DSA CPP

--- Dynamic Programming/maximum_sum_of_non-adjacent_elements.cpp
+++ Dynamic Programming/maximum_sum_of_non-adjacent_elements.cpp
@@ -32,13 +32,35 @@ int solver_dp(int *arr, int n  , vector<int> &dp){
     return dp[n];
 }
 
+//Tabulation 
+int solver_tabulation(int *arr, int n  , vector<int> &dp){
+    if (n < 0 ){
+        return 0 ;
+    }
+    if (n == 0){
+        return arr[0];
+    }
+    if (dp[n] != -1){
+        return dp[n];
+    }
+    for(int i  = 1 ; i < n ; i++){
+        int incl = dp[i -2] + arr[i];
+        int excl = dp[i-1] + 0;
+        dp[i] = max(incl , excl);
+    }
+    return dp[n-1];
+}
+
 int main(){
     int arr[] = {9 , 9 , 8, 2};
     int n = 4;
     vector<int> dp(n , -1);
     int ans = solver(arr , n -1);
     int dp_ans = solver_dp(arr , n-1  , dp);
+    dp[0] = arr[0];
+    int tab_ans = solver_tabulation(arr  , n -1 , dp);
     cout<< ans << endl;
-    cout<< dp_ans <<endl; 
+    cout<< dp_ans <<endl;
+    cout << tab_ans <<endl; 
     return 0;
 }

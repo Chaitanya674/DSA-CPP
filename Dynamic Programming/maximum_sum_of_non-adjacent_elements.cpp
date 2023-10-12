25330fdabf936d3e6ac2b5874d3fb1e02f701e88
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-10-12 16:50:35 +0530

    DSA CPP

--- Dynamic Programming/maximum_sum_of_non-adjacent_elements.cpp
+++ Dynamic Programming/maximum_sum_of_non-adjacent_elements.cpp
@@ -2,12 +2,23 @@
 
 using namespace std;
 
-int solver(){
-    return 0;
+int solver(int *arr, int n){
+    if (n < 0 ){
+        return 0;
+    }
+    if (n ==  0 ){
+        return arr[0];
+    }
+    int incl = solver(arr , n -2) + arr[n];
+    int excl = solver(arr , n-1) + 0;
+    return max(incl , excl);
 }
 
 
 int main(){
-    
+    int arr[] = {9 , 9 , 8, 2};
+    int n = 4;
+    int ans = solver(arr , n -1);
+    cout<< ans << endl;
     return 0;
 }
